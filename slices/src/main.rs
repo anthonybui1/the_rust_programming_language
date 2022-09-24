5a09fe50a925c0ce33f4e2fe1effb57f4381806c
Anthony Bui <nhut.anthony.bui@gmail.com>
2022-09-24 02:35:46 -0700

    Section 4 completed, ownership, references, borrowing, and slices

---
+++ slices/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let hello_world = String::from("Hello world!");
+
+    let hello = first_word(&hello_world);
+    println!("{}", hello);
+}
+
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
